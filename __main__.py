88aead43e21b3add57cc7f19dccbac2e0d2659b0
Avi Nehama <avi.nehama@gmail.com>
2021-09-30 10:59:32 +0000

    adds resourceGroup

--- __main__.py
+++ __main__.py
@@ -3,4 +3,5 @@ from pulumi_azure_native import resources, containerservice
 
 config = pulumi.Config()
 
-resource_group = resources.ResourceGroup(location="eastus", resource_group_name="zure-native-py-aks")
+resource_group = resources.ResourceGroup("myResourceGroup",
+    location="eastus", resource_group_name="zure-native-py-aks")
