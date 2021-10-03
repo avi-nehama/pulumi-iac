e3b0523b814e9c8c176b03af3543f7ec022837c5
Avi Nehama <avi.nehama@gmail.com>
2021-10-03 07:21:59 +0000

    adds cluster

--- __main__.py
+++ __main__.py
@@ -1,7 +1,17 @@
 import pulumi
-from pulumi_azure_native import resources, containerservice
+from pulumi_azure_native import resources, kubernetes
 
 config = pulumi.Config()
 
 resource_group = resources.ResourceGroup("myResourceGroup",
     location="eastus", resource_group_name="zure-native-py-aks")
+
+cluster = kubernetes.ConnectedCluster("connectedCluster",
+    cluster_name="testCluster",
+    identity=kubernetes.ConnectedClusterIdentityArgs(
+        type="SystemAssigned",
+    ),
+    location="East US",
+    resource_group_name="k8sc-rg",
+    tags={}
+)
